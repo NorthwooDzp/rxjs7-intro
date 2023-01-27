dec48b651e80336f52b3d39212072baf6193d9b6
Volodymyr L <northwoodzp@gmail.com>
2023-01-27 13:38:48 +0200

    Observable basic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer, Subscription } from "rxjs";
 
-const someObservable$ = new Observable<string>(subscriber => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const namesObservable$ = new Observable<string>((subscriber) => {
+  console.log("Names Observable executed");
+  subscriber.next("Alice");
+
+  setTimeout(() => {
+    subscriber.next("Ben");
+  }, 2000);
+  setTimeout(() => {
+    subscriber.next("Charlie");
+  }, 4000);
+
+  setTimeout(() => {
+    subscriber.error(new Error("Unhandled exception"));
+  }, 4500);
+
+  // subscriber.complete();
 });
 
-someObservable$.subscribe(value => console.log(value));
+const namesObserver: Partial<Observer<string>> = {
+  next: (value: string) => {
+    console.log(value);
+  },
+  error: (err) => {
+    console.log(err);
+  },
+  complete: () => {
+    console.log("observable completed");
+  },
+};
+
+const namesSubs: Subscription = namesObservable$.subscribe(namesObserver);
+
+const namesSubs2: Subscription = namesObservable$.subscribe(namesObserver);
+
+setTimeout(() => {
+  console.log("Unsubscribe");
+  namesSubs.unsubscribe();
+  namesSubs2.unsubscribe();
+}, 10000);
+
+/**
+ * 004
+ */
